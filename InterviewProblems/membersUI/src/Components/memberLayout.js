6f28191a2abeb307c72d3392ddb841701fc446ca
ptravis765 <46458293+ptravis765@users.noreply.github.com>
2019-01-11 20:38:32 -0500

    Add files via upload

---
+++ InterviewProblems/membersUI/src/Components/memberLayout.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function memberLayout(props){
+	
+
+	return(
+		<div className="members-layout">
+			<p>{props.first_name}</p>
+		</div>
+	)
+}
+
+export default memberLayout
