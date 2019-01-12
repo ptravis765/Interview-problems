09e75c2df013cb33072334babb21e9148300545b
ptravis765 <46458293+ptravis765@users.noreply.github.com>
2019-01-12 14:15:07 -0500

    Add files via upload

---
+++ TestCodeDoneToUnderstandReact/ippsec/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from "./HelloWorld";
+import "./index.css";
+
+
+
+ReactDOM.render(
+	<HelloWorld/>,
+	document.getElementById('root')
+);
